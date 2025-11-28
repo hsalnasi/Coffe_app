15556b02cffad3306107997b04bd5cb04c160634
hsalnasi <iihaifouz@gmail.com>
2025-11-28 22:24:53 +0300

    initilized my first chnage to my first flutter app ::)

---
+++ coffe_project/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+      title: Text("Hello :)"),
+      backgroundColor: const Color.fromARGB(255, 100, 72, 62),
+      centerTitle: true)
+      , body: Text("Welcome to Coffee App")
+     
+
+      
+      
+      )
+      
+       
+
+  ));
+}
+
